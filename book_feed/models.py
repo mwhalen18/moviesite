972efb8dcfe039a74339b73817a5597eeac34f2e
Matthew Whalen <matthewwhalen@Matthews-MacBook-Pro.local>
2023-08-03 11:56:14 -0500

    added book model

--- book_feed/models.py
+++ book_feed/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    isbn = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=1000)
+    authors = models.TextField(default='')
+    pub_date = models.CharField(max_length=10)
+    description = models.TextField(default='')
+    cover_path = models.URLField(max_length=200, default='')
